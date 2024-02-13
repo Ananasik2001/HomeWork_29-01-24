61e9d59e922525d7bb525b47c848a80eac9d7e1f
Alex <pgoppng@mail.ru>
2024-02-13 22:32:57 +0300

    Решение второй задачи

--- Program.cs
+++ Program.cs
@@ -4,3 +4,23 @@
 // {
 //     Console.WriteLine("+"); // Число кратно 7 и 23 (нацело делится)
 // }
+
+// №2
+int x = -2;
+int y = -3;
+if (x > 0 && y > 0)
+{
+    Console.WriteLine("I"); // 1 координатная четверть
+}
+if (x < 0 && y > 0)
+{
+    Console.WriteLine("II"); // 2 координатная четверть
+}
+if (x < 0 && y < 0)
+{
+    Console.WriteLine("III"); // 3 координатная четверть
+}
+if (x > 0 && y < 0)
+{
+    Console.WriteLine("IV"); // 4 координатная четверть
+}
