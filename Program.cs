770d85e5196b6a873eae17d137deb536a383761d
Alex <pgoppng@mail.ru>
2024-02-13 23:36:49 +0300

    решение третьей задачи

--- Program.cs
+++ Program.cs
@@ -6,21 +6,38 @@
 // }
 
 // №2
-int x = -2;
-int y = -3;
-if (x > 0 && y > 0)
-{
-    Console.WriteLine("I"); // 1 координатная четверть
-}
-if (x < 0 && y > 0)
-{
-    Console.WriteLine("II"); // 2 координатная четверть
-}
-if (x < 0 && y < 0)
-{
-    Console.WriteLine("III"); // 3 координатная четверть
-}
-if (x > 0 && y < 0)
-{
-    Console.WriteLine("IV"); // 4 координатная четверть
-}
+// int x = -2;
+// int y = -3;
+// if (x > 0 && y > 0)
+// {
+//     Console.WriteLine("I"); // 1 координатная четверть
+// }
+// if (x < 0 && y > 0)
+// {
+//     Console.WriteLine("II"); // 2 координатная четверть
+// }
+// if (x < 0 && y < 0)
+// {
+//     Console.WriteLine("III"); // 3 координатная четверть
+// }
+// if (x > 0 && y < 0)
+// {
+//     Console.WriteLine("IV"); // 4 координатная четверть
+// }
+
+// №3 
+// int number = 99;
+// int negativeN = 10;
+// int firstDigit = number / 10; // 96 / 10 = 9
+// int thirdDigit = number % 10; // 96 % 10 = 6
+// if(number >= negativeN)
+// {
+// if (firstDigit >= thirdDigit)
+// {
+//     Console.WriteLine ($"Результат от числа {number} => {firstDigit}");
+// }
+// else
+// {
+//     Console.WriteLine ($"Результат от числа {number} => {thirdDigit}");
+// }
+// }
